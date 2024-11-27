a2759447cc8bb7fe930df8b2f87d8d052b5740e7
Yorian Fransz <syndey-cook@outlook.com>
2024-11-27 15:08:50 +0100

    modify

--- hooks-exercise-starter/src/App.jsx
+++ hooks-exercise-starter/src/App.jsx
@@ -1,12 +1,13 @@
-import { useEffect } from "react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import UserDetail from "./UserDetail";
 
 const App = () => {
   const [userList, setUserList] = useState([]);
+  const [selectedUser, setSelectedUser] = useState(null);
+
   useEffect(() => {
     async function fetchUsers() {
       const response = await fetch("http://localhost:3000/users");
-
       const users = await response.json();
       setUserList(users);
     }
@@ -18,9 +19,22 @@ const App = () => {
       <h1>React Hooks Exercise Starter</h1>
       <ul>
         {userList.map((user) => (
-          <li key={user.id}>{user.name}</li>
+          <li
+            key={user.id}
+            onClick={() => setSelectedUser(user)}
+            style={{ cursor: "pointer" }}
+          >
+            {user.name}
+          </li>
         ))}
       </ul>
+
+      {selectedUser && (
+        <div>
+          <h2>Selected User</h2>
+          <UserDetail user={selectedUser} />
+        </div>
+      )}
     </div>
   );
 };
